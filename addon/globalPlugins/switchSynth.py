2f1672f73c9584f1f1b8b9a90816000413444a15
Tyler Spivey <tspivey@pcdesk.net>
2013-08-04 06:12:31 -0700

    Version 1.0 using addon template

---
+++ addon/globalPlugins/switchSynth.py
@@ -0,0 +1,56 @@
+import cPickle
+import os
+import config
+import globalPluginHandler
+import speech
+import ui
+
+class GlobalPlugin(globalPluginHandler.GlobalPlugin):
+
+	def __init__(self, *args, **kwargs):
+		super(GlobalPlugin, self).__init__(*args, **kwargs)
+		self.synths = {}
+		self.slot = 1
+		self.load()
+
+	def script_setSynth(self, gesture):
+		self.slot = slot = int(gesture.displayName[-1])
+		if slot in self.synths:
+			config.conf['speech'][self.synths[slot]['name']].clear()
+			config.conf['speech'][self.synths[slot]['name']].update(self.synths[slot]['config'])
+			speech.setSynth(self.synths[slot]['name'])
+		speech.getSynth().saveSettings()
+		ui.message(str(slot))
+	script_setSynth.__doc__ = _("Sets the currently active synthesizer to the selected slot.")
+
+	def script_saveSynth(self, gesture):
+		if self.slot not in self.synths:
+			self.synths[self.slot] = {}
+		self.synths[self.slot]['name'] = speech.getSynth().name
+		self.synths[self.slot]['config'] = dict(config.conf['speech'][speech.getSynth().name])
+		self.write()
+		ui.message(_("saved"))
+	script_saveSynth.__doc__ = _("Save the currently used synthesizer and its settings to the currently selected slot")
+
+	def write(self):
+		path = os.path.join(config.getUserDefaultConfigPath(), "switch_synth.pickle")
+		with open(path, 'wb') as f:
+			cPickle.dump(self.synths, f)
+
+	def load(self):
+		path = os.path.join(config.getUserDefaultConfigPath(), "switch_synth.pickle")
+		if not os.path.exists(path): return
+		with open(path, 'rb') as f:
+			self.synths = cPickle.load(f)
+		if 'version' not in self.synths:
+			self.synths = {'version': 1}
+
+	__gestures = {
+	"kb:control+NVDA+1": "setSynth",
+	"kb:control+NVDA+2": "setSynth",
+	"kb:control+NVDA+3": "setSynth",
+	"kb:control+NVDA+4": "setSynth",
+	"kb:control+NVDA+5": "setSynth",
+	"kb:control+NVDA+6": "setSynth",
+	"kb:control+shift+NVDA+v":"saveSynth",
+}
---
+++ buildVars.py
@@ -0,0 +1,44 @@
+# -*- coding: UTF-8 -*-
+
+# Build customizations
+# Change this file instead of sconstruct or manifest files, whenever possible.
+
+# Full getext (please don't change)
+_ = lambda x : x
+
+# Add-on information variables
+addon_info = {
+	# for previously unpublished addons, please follow the community guidelines at:
+	# https://bitbucket.org/nvdaaddonteam/todo/src/56140dbec531e4d7591338e1dbc6192f3dd422a8/guideLines.txt
+	# add-on Name, internal for nvda
+	"addon-name" : "switchSynth",
+	# Add-on summary, usually the user visible name of the addon.
+	# TRANSLATORS: Summary for this add-on to be shown on installation and add-on information.
+	"addon-summary" : _("Switch synth"),
+	# Add-on description
+	# Translators: Long description to be shown for this add-on on add-on information from add-ons manager
+	"addon-description" : _("""Allows easy switching between synthesizers.
+To switch synthesizers, press control+NVDA+1 through control+NVDA+6.
+To save the current voice in the currently selected slot, press control+shift+NVDA+v."""
+	),
+	# version
+	"addon-version" : "1.0-dev",
+	# Author(s)
+	"addon-author" : "Tyler Spivey <tspivey@pcdesk.net>",
+	# URL for the add-on documentation support
+	"addon-url" : None
+}
+
+
+import os.path
+
+# Define the python files that are the sources of your add-on.
+# You can use glob expressions here, they will be expanded.
+pythonSources = ["addon/globalPlugins/switchSynth.py"]
+
+# Files that contain strings for translation. Usually your python sources
+i18nSources = pythonSources + ["buildVars.py", "docHandler.py"]
+
+# Files that will be ignored when building the nvda-addon file
+# Paths are relative to the addon directory, not to the root directory of your addon sources.
+excludedFiles = []
